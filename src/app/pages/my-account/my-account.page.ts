680a3c2cadaf6396a586a8ea22744d8f9f0e1468
Ahmed Mujtaba <kahmedmujtaba@gmail.com>
2019-08-06 23:06:37 +0500

    header centered fixed

--- src/app/pages/my-account/my-account.page.ts
+++ src/app/pages/my-account/my-account.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-account.page.scss'],
 })
 export class MyAccountPage implements OnInit {
+  title = "My Account";
 
   constructor() { }
 
